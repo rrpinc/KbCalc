e2a629a7bcdad97ee85ec27baa6a8a890b06b6ef
roni <roni@sears.co.il>
2016-03-05 23:33:32 +0200

    Initial Commit

---
+++ KbCalc/KbCalc.h
@@ -0,0 +1,19 @@
+//
+//  KbCalc.h
+//  KbCalc
+//
+//  Created by Roni Poyas on 05/03/2016.
+//  Copyright © 2016 rrpinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KbCalc.
+FOUNDATION_EXPORT double KbCalcVersionNumber;
+
+//! Project version string for KbCalc.
+FOUNDATION_EXPORT const unsigned char KbCalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KbCalc/PublicHeader.h>
+
+
